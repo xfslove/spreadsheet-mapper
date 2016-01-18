92f3c7b0887804841576003513bfd91566d31ff3
hanwen.huang <xfslove@gmail.com>
2016-01-18 11:22:37 +0800

    update

---
+++ src/main/java/me/excel/tools/ExcelConstants.java
@@ -0,0 +1,14 @@
+package me.excel.tools;
+
+/**
+ * Created by hanwen on 16-1-18.
+ */
+public class ExcelConstants {
+
+  private ExcelConstants() {
+  }
+
+  public static final String SUFFIX_XLS = ".xls";
+
+  public static final String SUFFIX_XLSX = ".xlsx";
+}
