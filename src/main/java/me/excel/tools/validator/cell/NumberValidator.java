9f78d7403b087d4fc7a19c1c64019f7fde0f9a40
hanwen.huang <xfslove@gmail.com>
2016-07-07 15:24:04 +0800

    add number validator

---
+++ src/main/java/me/excel/tools/validator/cell/NumberValidator.java
@@ -0,0 +1,23 @@
+package me.excel.tools.validator.cell;
+
+import me.excel.tools.model.excel.ExcelCell;
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * Created by hanwen on 16/7/7.
+ */
+public class NumberValidator extends AbstractCellValidator {
+
+  public NumberValidator(String field) {
+    super(field, "应该为数字", "数字");
+  }
+
+  public NumberValidator(String field, String message, String prompt) {
+    super(field, message, prompt);
+  }
+
+  @Override
+  protected boolean customValidate(ExcelCell excelCell) {
+    return NumberUtils.isNumber(excelCell.getValue());
+  }
+}
