08da716b2041c1379764e812c0da7969fb2a3e57
hanwen.huang <xfslove@gmail.com>
2017-01-11 17:43:23 +0800

    move field meta builder to inner class

--- src/main/java/spreadsheet/mapper/o2w/compose/builder/SequenceBasedSheetMetaBuilder.java
+++ src/main/java/spreadsheet/mapper/o2w/compose/builder/SequenceBasedSheetMetaBuilder.java
@@ -1,8 +1,6 @@
 package spreadsheet.mapper.o2w.compose.builder;
 
-import spreadsheet.mapper.model.meta.FieldMeta;
-import spreadsheet.mapper.model.meta.SheetMeta;
-import spreadsheet.mapper.model.meta.SheetMetaBean;
+import spreadsheet.mapper.model.meta.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +13,8 @@ import java.util.List;
 public class SequenceBasedSheetMetaBuilder {
 
   private int columnIndex = 1;
-  int rowIndex = 1;
-  List<FieldMeta> fieldMetas = new ArrayList<>();
+  private int rowIndex = 1;
+  private List<FieldMeta> fieldMetas = new ArrayList<>();
 
   /**
    * create a field meta and add at {@link #columnIndex}, after add {@link #columnIndex} will plus 1
@@ -27,7 +25,6 @@ public class SequenceBasedSheetMetaBuilder {
    */
   public SequenceBasedFieldMetaBuilder field(String prefix, String name) {
     SequenceBasedFieldMetaBuilder sequenceBasedFieldMetaBuilder = new SequenceBasedFieldMetaBuilder(prefix, name, columnIndex);
-    sequenceBasedFieldMetaBuilder.setSequenceBasedSheetMetaBuilder(this);
     columnIndex++;
     return sequenceBasedFieldMetaBuilder;
   }
@@ -92,5 +89,80 @@ public class SequenceBasedSheetMetaBuilder {
       sheetMeta.addFieldMeta(fieldMeta);
     }
     return sheetMeta;
+  }
+
+  /**
+   * {@link FieldMeta} builder
+   */
+  public class SequenceBasedFieldMetaBuilder {
+
+    private int rowIndex = 1;
+    private FieldMeta fieldMeta;
+
+    private SequenceBasedFieldMetaBuilder(String prefix, String name, int columnIndex) {
+      this.fieldMeta = new FieldMetaBean(prefix, name, columnIndex);
+    }
+
+    /**
+     * create a header meta and add at {@link #rowIndex}, after add {@link #rowIndex} will plus 1
+     *
+     * @param value {@link HeaderMeta#getValue()}
+     * @return this
+     */
+    public SequenceBasedFieldMetaBuilder header(String value) {
+      headers(value);
+      return this;
+    }
+
+    /**
+     * add header meta by sequence
+     *
+     * @param values {@link HeaderMeta#getValue()}
+     * @return this
+     * @see #header(String)
+     */
+    public SequenceBasedFieldMetaBuilder headers(String... values) {
+      if (values == null) {
+        return this;
+      }
+      for (String value : values) {
+        fieldMeta.addHeaderMeta(new HeaderMetaBean(rowIndex, value));
+        rowIndex++;
+      }
+      return this;
+    }
+
+    /**
+     * skip one row
+     *
+     * @return this
+     */
+    public SequenceBasedFieldMetaBuilder skip() {
+      skip(1);
+      return this;
+    }
+
+    /**
+     * skip supplied numbers rows
+     *
+     * @param rowNum skip how much columns
+     * @return this
+     */
+    public SequenceBasedFieldMetaBuilder skip(int rowNum) {
+      rowIndex += rowNum;
+      return this;
+    }
+
+    /**
+     * finish one field create, go to next
+     *
+     * @return {@link SequenceBasedSheetMetaBuilder}
+     */
+    public SequenceBasedSheetMetaBuilder next() {
+      SequenceBasedSheetMetaBuilder.this.rowIndex = Math.max(this.rowIndex, SequenceBasedSheetMetaBuilder.this.rowIndex);
+      SequenceBasedSheetMetaBuilder.this.fieldMetas.add(fieldMeta);
+      return SequenceBasedSheetMetaBuilder.this;
+    }
+
   }
 }
